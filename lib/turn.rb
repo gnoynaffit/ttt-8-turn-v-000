4f553e268a818de7a1a7b5afaf0249e70515c732
Tiffany <ong.tifffany1503@gmail.com>
2018-09-11 16:22:46 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,21 @@
+#display a tic tac toe board
+def display_board(board)
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "----------="
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+#Convert
+
+
+
+
+
+
+
+
+
+
+
+
