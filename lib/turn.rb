5995333847ece273ae26a6e2e3f3467ebc93e4a4
Tiffany <ong.tifffany1503@gmail.com>
2018-09-11 17:00:36 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,7 +32,7 @@ def on_board?(num)
  end
   end
 
-  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+  if position_taken?(board, index) == false && on_board?(index) == true
     return true
   else
     return false
