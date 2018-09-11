34b4a5c5968f5a7b9fc01600a70ef9c06c7e71ef
Tiffany <ong.tifffany1503@gmail.com>
2018-09-11 16:37:57 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,7 +17,7 @@ end
 #valid move
 def valid_move?(board, index)
  def position_taken?(array, ind)
-   if array[ind] == "" || array[ind] == "" || array[ind] == nil
+   if array[ind] == " " || array[ind] == "" || array[ind] == nil
      return false
    else
      return true
